4ba3bde2fc641114c115e8506f14a2b955636e99
Bruno Barange <bruno.akun@gmail.com>
2022-09-09 13:53:29 +0200

    ruta y capturar id de la URL

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroe.component.scss']
 })
 export class HeroeComponent implements OnInit {
+  auxId: string | any = '';
 
-  constructor() { }
+  constructor(
+    public rutaActiva: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.auxId = this.rutaActiva.snapshot.paramMap.get('id');
+
+    console.log(this.auxId);
+
   }
 
 }
